1ddff89e44c25002618786a93905e8d342d289eb
darrenwong916 <darrenwong06@gmail.com>
2014-03-14 12:32:31 -0700

    changed to rails application

---
+++ app/controllers/tictac_controller.rb
@@ -0,0 +1,8 @@
+class TictacController < ApplicationController
+
+def index
+end
+
+
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TictactoeProject::Application.config.secret_key_base = '05662fc6ec1c7803b03e2c3de4a1b774898350346919864ccfd5845bc10e08e39ac1761ec08362734002d19cd47875557a423bc28434a49f07a70062b0b75d45'
