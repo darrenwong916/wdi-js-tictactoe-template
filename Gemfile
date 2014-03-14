bb7b62376206820b70b961b00c833344edf10624
darrenwong916 <darrenwong06@gmail.com>
2014-03-14 12:45:46 -0700

    css and gemfiles installed into rails application

--- Gemfile
+++ Gemfile
@@ -6,11 +6,7 @@ gem 'rails', '4.0.2'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
 
-group :assets do
-
 gem 'angularjs-rails'
-
-end
 
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 4.0.0'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 TictactoeProject::Application.routes.draw do
-  # root to: "/"
+  root :to => 'tictac#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
